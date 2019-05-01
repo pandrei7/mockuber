3c3265ff1355d35790f8ac9763e8862f4e803cfb
Preda Andrei <preda.andrei174@gmail.com>
2019-05-01 23:42:27 +0300

    Add Graph class

---
+++ graph.h
@@ -0,0 +1,97 @@
+// Copyright 2019 Preda Andrei
+
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <algorithm>
+#include <cstddef>
+#include <vector>
+
+template <typename T>
+class Graph
+{
+ public:
+    struct Node
+    {
+        T data;
+        std::vector<int> edges;
+
+        explicit Node(const T &data) : data(data) {}
+    };
+
+    void AddNode(const T &data)
+    {
+        nodes_.push_back(Node(data));
+    }
+
+    bool AddEdge(int a, int b)
+    {
+        auto &edges = nodes_[a].edges;
+        auto it = find(edges.begin(), edges.end(), b);
+
+        if (it == edges.end()) {
+            edges.push_back(b);
+            return true;
+        }
+        return false;
+    }
+
+    bool RemoveEdge(int a, int b)
+    {
+        auto &edges = nodes_[a].edges;
+        auto it = find(edges.begin(), edges.end(), b);
+
+        if (it != edges.end()) {
+            edges.erase(it);
+            return true;
+        }
+        return false;
+    }
+
+    void ToggleEdge(int a, int b)
+    {
+        auto &edges = nodes_[a].edges;
+        auto it = find(edges.begin(), edges.end(), b);
+
+        if (it == edges.end()) {
+            edges.push_back(b);
+        } else {
+            edges.erase(it);
+        }
+    }
+
+    Graph Transposed() const
+    {
+        Graph trans;
+        for (const auto &node : nodes_) {
+            trans.AddNode(node.data);
+        }
+
+        for (std::size_t i = 0; i < nodes_.size(); i += 1) {
+            for (const auto &other : nodes_[i].edges) {
+                trans.AddEdge(other, i);
+            }
+        }
+        return trans;
+    }
+
+    std::size_t Size() const
+    {
+        return nodes_.size();
+    }
+
+    T& Data(int index)
+    {
+        return nodes_[index].data;
+    }
+
+    const std::vector<int>& Edges(int index)
+    {
+        return nodes_[index].edges;
+    }
+
+ private:
+    std::vector<Node> nodes_;
+};
+
+#endif  // GRAPH_H_
---
+++ test_graph.cpp
@@ -0,0 +1,151 @@
+// Copyright 2019 Preda Andrei
+
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "catch.hpp"
+
+#include "graph.h"
+
+TEST_CASE("Graph has correct size")
+{
+    Graph<std::string> graph;
+    REQUIRE(graph.Size() == 0);
+
+    graph.AddNode("Ala");
+    REQUIRE(graph.Size() == 1);
+
+    graph.AddNode("Bala");
+    REQUIRE(graph.Size() == 2);
+
+    graph.AddNode("Portocala");
+    REQUIRE(graph.Size() == 3);
+}
+
+TEST_CASE("Data is held correctly inside the nodes")
+{
+    std::vector<std::string> names = {
+        "First", "Second", "Third", "Fourth"
+    };
+
+    Graph<std::string> graph;
+    for (const auto &name : names) {
+        graph.AddNode(name);
+    }
+
+    REQUIRE(graph.Size() == names.size());
+    for (std::size_t i = 0; i < graph.Size(); i += 1) {
+        REQUIRE(graph.Data(i) == names[i]);
+    }
+
+    SECTION("data can be modified") {
+        for (std::size_t i = 0; i < graph.Size(); i += 1) {
+            graph.Data(i) = names[names.size() - i - 1];
+        }
+        for (std::size_t i = 0; i < graph.Size(); i += 1) {
+            REQUIRE(graph.Data(i) == names[names.size() - i - 1]);
+        }
+    }
+}
+
+TEST_CASE("Edge-operations work correctly")
+{
+    Graph<std::string> graph;
+    graph.AddNode("A");
+    graph.AddNode("B");
+    graph.AddNode("C");
+
+    graph.AddEdge(0, 1);
+    REQUIRE(graph.Edges(0).size() == 1);
+    REQUIRE(graph.Edges(0)[0] == 1);
+
+    graph.AddEdge(1, 2);
+    REQUIRE(graph.Edges(1).size() == 1);
+    REQUIRE(graph.Edges(1)[0] == 2);
+
+    graph.AddEdge(2, 0);
+    graph.AddEdge(2, 1);
+    REQUIRE(graph.Edges(2).size() == 2);
+    REQUIRE(graph.Edges(2)[0] == 0);
+    REQUIRE(graph.Edges(2)[1] == 1);
+
+    SECTION("multiple edges are not allowed") {
+        graph.AddEdge(0, 1);
+        graph.AddEdge(0, 1);
+        graph.AddEdge(0, 1);
+        REQUIRE(graph.Edges(0).size() == 1);
+        REQUIRE(graph.Edges(0)[0] == 1);
+    }
+
+    SECTION("self loops are allowed") {
+        graph.AddEdge(0, 0);
+        graph.AddEdge(0, 0);
+        REQUIRE(graph.Edges(0).size() == 2);
+        REQUIRE(graph.Edges(0)[1] == 0);
+    }
+
+    SECTION("edges are removed correctly") {
+        SECTION("remove existing edges") {
+            graph.RemoveEdge(0, 1);
+
+            REQUIRE(graph.Edges(0).size() == 0);
+            REQUIRE(graph.Edges(1).size() == 1);
+            REQUIRE(graph.Edges(2).size() == 2);
+        }
+
+        SECTION("remove non-existing edges") {
+            graph.RemoveEdge(0, 2);
+
+            REQUIRE(graph.Edges(0).size() == 1);
+            REQUIRE(graph.Edges(0)[0] == 1);
+
+            REQUIRE(graph.Edges(2).size() == 2);
+            REQUIRE(graph.Edges(2)[0] == 0);
+            REQUIRE(graph.Edges(2)[1] == 1);
+        }
+    }
+
+    SECTION("edges are toggled correctly") {
+        graph.ToggleEdge(0, 1);
+        REQUIRE(graph.Edges(0).size() == 0);
+        REQUIRE(graph.Edges(1).size() == 1);
+
+        graph.ToggleEdge(1, 0);
+        REQUIRE(graph.Edges(0).size() == 0);
+        REQUIRE(graph.Edges(1).size() == 2);
+        REQUIRE(graph.Edges(1)[1] == 0);
+
+        graph.ToggleEdge(0, 1);
+        REQUIRE(graph.Edges(0).size() == 1);
+        REQUIRE(graph.Edges(0)[0] == 1);
+    }
+}
+
+TEST_CASE("Transposed works correctly")
+{
+    std::vector<std::pair<int, int>> edges = {
+        {0, 1}, {1, 2}, {2, 0}, {2, 1}
+    };
+
+    Graph<std::string> graph;
+    for (auto i = 0; i < 3; i += 1) {
+        graph.AddNode("A");
+    }
+    for (const auto &p : edges) {
+        graph.AddEdge(p.first, p.second);
+    }
+
+    auto trans = graph.Transposed();
+    REQUIRE(trans.Size() == graph.Size());
+
+    REQUIRE(trans.Edges(0).size() == 1);
+    REQUIRE(trans.Edges(1).size() == 2);
+    REQUIRE(trans.Edges(2).size() == 1);
+
+    const auto &edges1 = trans.Edges(1);
+    REQUIRE(find(edges1.begin(), edges1.end(), 0) != edges1.end());
+    REQUIRE(find(edges1.begin(), edges1.end(), 2) != edges1.end());
+    REQUIRE(find(edges1.begin(), edges1.end(), 5) == edges1.end());
+}
